c4ac80e07f5e082549e4c12b85286f14cca89224
reynbot <natividad.reynard@outlook.ph>
2018-07-21 13:19:39 +0800

    Test commit

--- devicetracker/Controllers/HomeController.cs
+++ devicetracker/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace devicetracker.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hello World";
 
             return View();
         }
